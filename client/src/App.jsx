43312412aa6663f5130760ce0eb285b7eb07d72a
UtkarshAnand07 <utkarshanand7193@gmail.com>
2025-03-05 23:39:20 +0530

    added login page

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import FloatingShape from "./components/FloatingShape";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+
+function App() {
+  return (
+    <>
+      <div
+        className='min-h-screen bg-gradient-to-br from-blue-950 via-indigo-900 to-blue-800 flex items-center justify-center relative overflow-hidden'
+      >
+        <FloatingShape color='bg-blue-500' size='w-64 h-64' top='-5%' left='10%' delay={0} />
+        <FloatingShape color='bg-indigo-500' size='w-48 h-48' top='70%' left='80%' delay={5} />
+        <FloatingShape color='bg-cyan-500' size='w-32 h-32' top='40%' left='-10%' delay={2} />
+
+        <Routes>
+          <Route path='/' element={"Home"} />
+          <Route path='/signup' element={<SignUpPage />} />
+          <Route path='/login' element={<LoginPage />} />
+        </Routes>
+      </div>
+    </>
+  )
+}
+
+export default App;
