3d60a34dff39ce166eaadab011cbd7d988410193
UtkarshAnand07 <utkarshanand7193@gmail.com>
2025-03-07 00:58:06 +0530

    added deploy

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import { connectDB } from "./db/connectDB.js";
 
@@ -11,6 +12,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
@@ -18,6 +20,14 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "../client/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
+	});
+}
 
 app.listen(PORT, () => {
 	connectDB();
